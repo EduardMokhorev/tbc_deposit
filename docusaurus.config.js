4348f971e2d9922066c8de76c39b4046d169b393
Eduard <101931036+EduardMokhorev@users.noreply.github.com>
2024-12-06 11:50:33 +0300

    Add files via upload

---
+++ docusaurus.config.js
@@ -0,0 +1,95 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+const simplePlantUML = require("@akebifiky/remark-simple-plantuml"); // объявляем плагин для plantuml
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'tbc_deposit',
+  tagline: 'документация',
+  favicon: 'img/favicon.ico',
+  url: 'https://EduardMokhorev.github.io/', // Адрес gitlab pages, обычно https://<username>.github.io
+  baseUrl: 'tbc_deposit',
+  organizationName: 'EduardMokhorev',
+  projectName: 'tbc_deposit', 
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+  trailingSlash: false,
+  deploymentBranch: 'gh-pages',
+
+//подключаем плагин для drawio
+plugins: [
+],
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // Useful options to enforce blogging best practices
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'deposit service',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/facebook/docusaurus',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()} Все права защищены.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
